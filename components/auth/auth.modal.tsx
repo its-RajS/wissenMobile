00a83f8ffd58ff48fd74c8e2eee1a93c91d72cc5
Raj <rajsharma222547@gmail.com>
2026-03-01 23:41:20 +0530

    build:auth setup

--- components/auth/auth.modal.tsx
+++ components/auth/auth.modal.tsx
@@ -1,9 +1,32 @@
 import { fontSizes, windowHeight, windowWidth } from '@/themes/app.constants'
+import { GoogleSignin } from '@react-native-google-signin/google-signin'
 import { BlurView } from 'expo-blur'
 import React from 'react'
-import { Image, Pressable, Text, View } from 'react-native'
+import { Image, Platform, Pressable, Text, View } from 'react-native'
+
 
 export default function AuthModal({ setModalVisible }: { setModalVisible: (val: boolean) => void }) {
+
+    const configureGoogleSignIn = () => {
+        try {
+            if (Platform.OS === 'ios') {
+                GoogleSignin.configure({
+                    iosClientId: process.env.EXPO_PUBLIC_IOS__GOOGLE_API_KEY,
+                });
+            } else {
+                GoogleSignin.configure({
+                    webClientId: "YOUR_WEB_CLIENT_ID",
+                });
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const googleSignIn = async () => {
+        console.log("Google Sign In");
+    }
+
     return (
         <BlurView intensity={10} style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
             <Pressable style={{
@@ -43,7 +66,9 @@ export default function AuthModal({ setModalVisible }: { setModalVisible: (val: 
                         gap: windowWidth(20),
                     }}
                 >
-                    <Pressable>
+                    <Pressable
+                        onPress={() => googleSignIn()}
+                    >
                         <Image
                             source={require("@/assets/images/onboarding/google.png")}
                             style={{
