0f46920168bfc39d72859c76c9e1b7e5e14a28cb
Raj <rajsharma222547@gmail.com>
2026-03-11 22:36:58 +0530

    build: resources screen

--- screens/resourses/resources.screen.tsx
+++ screens/resourses/resources.screen.tsx
@@ -1,11 +1,15 @@
+import SourceCodeCard from "@/components/cards/sourceCode";
+import { videoLessonsData } from "@/config/constants";
 import { useTheme } from "@/context/theme.context";
 import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
 import React from "react";
 import {
-    ScrollView,
-    View
+  FlatList,
+  ScrollView,
+  View
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { verticalScale } from "react-native-size-matters";
   
   export default function ResourcesScreen() {
     const { theme } = useTheme();
@@ -21,7 +25,14 @@ import { SafeAreaView } from "react-native-safe-area-context";
         <ScrollView>
 
           <View style={{ paddingBottom: bottomTabBarHeight - 20 }}>
-            
+          <FlatList
+            data={videoLessonsData}
+            renderItem={({ item }) => <SourceCodeCard item={item} />}
+            showsVerticalScrollIndicator={false}
+            style={{
+              paddingTop: verticalScale(10),
+            }}
+          />
           </View>
         </ScrollView>
       </SafeAreaView>
