9fc14c77657dcfe090db5990d9ea8e869388e015
Raj <rajsharma222547@gmail.com>
2026-03-11 12:24:18 +0530

    build: resource screen setup

--- screens/resourses/resources.screen.tsx
+++ screens/resourses/resources.screen.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { useTheme } from "@/context/theme.context";
+import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
+import React from "react";
+import {
+    ScrollView,
+    View
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+  
+  export default function ResourcesScreen() {
+    const { theme } = useTheme();
+    const bottomTabBarHeight = useBottomTabBarHeight();
+  
+    return (
+      <SafeAreaView
+        style={{
+          backgroundColor: theme.dark ? "#131313" : "#fff",
+          flex: 1,
+        }}
+      >
+        <ScrollView>
 
-export default function ResoursesScreen() {
-  return (
-    <View>
-      <Text>ResoursesScreen</Text>
-    </View>
-  )
-}
+          <View style={{ paddingBottom: bottomTabBarHeight - 20 }}>
+            
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    );
+  }
+  
