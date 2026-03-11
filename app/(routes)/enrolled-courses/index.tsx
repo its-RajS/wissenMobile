2106261c85b4f46c6fbd86c4ef9b5f6ffe6d7391
Raj <rajsharma222547@gmail.com>
2026-03-11 11:47:53 +0530

    build: profile screen and fix issue of login due to network ip

---
+++ app/(routes)/enrolled-courses/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function EnrolledCourses() {
+  return (
+    <View>
+      <Text>EnrolledCourses</Text>
+    </View>
+  )
+}
